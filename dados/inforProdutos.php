2465fa35c1becfbd0df206ec55c55bec37183010
Marcus Vinicius Jacome <86629665+markabc01@users.noreply.github.com>
2021-08-27 20:34:07 -0300

    Add files via upload

---
+++ dados/inforProdutos.php
@@ -0,0 +1,56 @@
+<?php
+	$produtos = array(
+		   array("titulo" => "COCADA", "informacao" => "COCADA é feito com coco, castanha de caju e leite condensado, <br>
+			deliciosa e crocante.<br>
+			 Sabor macio e docinho que derrete na boca, sabor inconfundível.  Tradicional em várias regiões do mundo, especialmente na América Latina.", "valor" => "1,99", "img" => "images/01cocada.jpg"),
+
+		   array("titulo" => "DOCE DE BANANA", "informacao" => "DOCE DE BANANA é feito com pedaços de banana e <br>
+			açúcar fervidos em água, com uma pitada de canela. Gostoso, simples e tradicional.<br>
+			 Perfeito para aqueles dias que bate vontade de comer uma coisinha diferente.", "valor" => "7,60", "img" => "images/02docedebanana.jpg"),
+
+		   array("titulo" => "TRUFA", "informacao" => "TRUFA é feito com chocolate e colocado um recheio feito de creme de<br>
+			  leite e conhaque, junto com noz, avelã, gotas de licor.<br> É coberto por chocolate e envolta no cacau em pó.<br>
+			 Bastante popular e cada vez agrada mais os paladares exigentes.", "valor" => "3,50", "img" => "images/03trufa.jpg"),
+
+		   array("titulo" => "PÃO DE MEL", "informacao" => "PÃPO DE MEL é feito com mel, farinha de trigo, chocolate, manteiga, especiarias e ovos, <br>
+			além da deliciosa cobertura de chocolate.<br>
+			 Saboroso feito em qualquer lugar do mundo.", "valor" => "4,00", "img" => "images/04paodemel.jpg"),
+
+		   array("titulo" => "SEQUILHOS", "informacao" => "SEQUILHOS, os nossos são feitos com amido de milho, leite condensado e ovos. <br>
+			Os famosos biscoitinhos doces de vovó, sequilhos, são uma verdadeira delícia!<br>
+			 Mais ainda quando eles derretem na boca. Experimente!", "valor" => "4,70", "img" => "images/05sequilhos.jpg"),
+
+		   array("titulo" => "QUINDIM", "informacao" => "QUINDIM é feito com gema de ovo, açúcar e coco ralado. Ficando dourado e macio. <br>
+			Elaborado em solo português, adaptado por africanas<br>
+			 em solo brasileiro e saboreado por todos!", "valor" => "8,50", "img" => "images/06quindim.jpg"),
+
+		 array("titulo" => "MINI PUDIM", "informacao" => "MINI PUDIM é feito à base de chocolate, frutas e preparada com leite, <br>
+			 leite condensado e ovos, e assada ou cozida em banho-maria em calda de açúcar queimado.<br>
+			 O pudim é uma delícia e conquistou há muitos anos os lares brasileiros.", "valor" => "4,15", "img" => "images/07pudim.jpg"),
+
+		   array("titulo" => "BOLO RECHEADO", "informacao" => "BOLO RECHEADO é feito a base de farinha e ovos, mas com diversos ingredientes adicionais, <br>
+			para deixar o recheio com o seu sabor preferido.<br>
+			 Difícil vai ser escolher o mais gostoso!", "valor" => "7,70", "img" => "images/08bolorecheado.jpg"),
+
+		   array("titulo" => "SONHO", "informacao" => "SONHO é feito com leite, leite condensado e ovos, com o tradicional<br>
+			  recheio de baunilha e açúcar polvilhado por cima.<br>
+			 Um dos doces mais queridos do Brasil. Redondinho e macio.", "valor" => "4,00", "img" => "images/09sonho.jpg"),
+
+		   array("titulo" => "BOLO DE ROLO", "informacao" => "BOLO DE ROLO é feito com farinha de trigo, ovos, manteiga e açúcar, <br>
+			é enrolada com uma camada de goiabada derretida.<br>
+			 Simplesmente irresistível que derrete na boca.", "valor" => "18,70", "img" => "images/10boloderolo.jpg"),
+
+
+		   array("titulo" => "PAÇOCA", "informacao" => "PAÇOCA é feito com amendoim, açúcar cristal e sal. <br>
+			A paçoca é rica em nutrientes antioxidantes e auxilia na prevenção de doenças cardiovasculares.<br>
+			 Uma delicia da culinaria brasileira.", "valor" => "2,00", "img" => "images/11pacoca.jpg"),
+
+		   array("titulo" => "COPO DA FELICIDADE", "informacao" => "COPO DA FELICIDADE é feito com tudo aquilo que mais gostar, <br>
+			é só usar a criatividade. Uma mistura de recheios, cremes, frutas, doces, confeitos, etc.<br>
+			 Uma tendência que ja conquistou o Brasil.", "valor" => "19,00", "img" => "images/12copodafelicidade.jpg"),
+
+
+		);
+
+
+ ?>
